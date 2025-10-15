94be882870428da05ea2468394456fea5ae8c615
DavidSRepo <roadhazard2@gmail.com>
2025-10-14 21:33:55 -0400

    Big update, lots of implementations, getting closer!

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { Pool } = require("pg");
+require("dotenv").config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.e.v.PGPORT,
+});
+
+
+
+app.use(express.static(path.join(_dirname, 'public')));
+
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(_dirname, "public", "index.html"));
+});
+
+
+app.post("/submit", async (req, res) => {
+    const { text } = req.body;
+    if(!text){
+        return res.status(480).json({error: 'No text provided'});
+    }
+
+    try {
+        await pool.query('INSERT INTO submissions (text_content) VALUES ($1)', [text]);
+        res.json({ succcess: true });
+    }
+
+    catch (err){
+        console.error('Database error:', err);
+        res.status(500).json({error: 'Database error: ' + err.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server running on port ${PORT}');
+})
